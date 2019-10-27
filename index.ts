823b44fbe2e077236ebdd6c056eb2cecf0160ef4
al2108 <alewark@gmail.com>
2019-10-27 09:34:47 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -30,15 +30,43 @@ class txCollect {
   public static tx = {};
 
   public static forTranslation(target: Object, propertyName: string) {
-    console.log(target, propertyName);
-    txCollect.modify(target[propertyName]);
+    // console.log(target, propertyName);
+    // const paths = txCollect.modify(target[propertyName], '');
+    // console.log(paths);
+    txCollect.modify2(target[propertyName]);
+    console.log(target[propertyName]);
     txCollect.tx = {...txCollect.tx, ...target[propertyName]};
-    console.log(txCollect.tx);
+    // console.log(txCollect.tx);
   }
 
-  public static modify(obj: Object) {
-
+  public static modify(obj: Object, parentKey: string) {
+    // console.log(Object.getOwnPropertyNames(obj));
+    let result;
+    if (_.isPlainObject(obj)) {
+      result = _.flatMap(_.keys(obj), (key) => {
+          return _.map(this.modify(obj[key], key), (subkey) => {
+              return (parentKey ? parentKey + '.' : '') + subkey;
+          });
+      });
+    }
+    else {
+      result = [];
+    }
+    return _.concat(result, parentKey || []);
   }
+
+  public static modify2(obj: Object, parentKey: string = '') {
+    // console.log('modify2:', obj, parentKey);
+    // console.log(_.keys(obj));
+    for (let key of _.keys(obj)) {
+      // console.log('modify2-key:', key);
+      if (_.isPlainObject(obj[key])) {
+        txCollect.modify2(obj[key], parentKey === '' ? key : parentKey + '.' + key);
+      } else if (typeof obj[key] === "string") {
+        obj[key] = parentKey + '.' + key;
+      }
+    }
+  } 
 
   public translate(path: string): string {
     return _.get(this.translations, path);
